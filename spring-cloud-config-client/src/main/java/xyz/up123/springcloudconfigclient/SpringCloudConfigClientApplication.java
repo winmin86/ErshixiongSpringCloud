de48c616c3def735c61d18f1551d4c0ecc821405
zhuwenming <zhuwenming@ybm100.com>
2019-09-27 16:18:16 +0800

    config集群模式完善

--- spring-cloud-config-client/src/main/java/xyz/up123/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/xyz/up123/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -1,31 +1,18 @@
 package xyz.up123.springcloudconfigclient;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-@RestController
-@RefreshScope
 public class SpringCloudConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConfigClientApplication.class, args);
     }
 
-    @Autowired
-    ConfigUtil configUtil;
 
-    @RequestMapping(value = "/hi")
-    public String hi(){
-
-        return configUtil.getFoo() + "," + configUtil.getMessage();
-    }
 }
