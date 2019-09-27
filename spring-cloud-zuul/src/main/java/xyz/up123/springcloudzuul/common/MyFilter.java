13c9b4c31ce67a8e99ffdb722363d150dbb15ee3
zhuwenming <zhuwenming@ybm100.com>
2019-09-27 11:15:45 +0800

    添加zuul模块

---
+++ spring-cloud-zuul/src/main/java/xyz/up123/springcloudzuul/common/MyFilter.java
@@ -0,0 +1,71 @@
+package xyz.up123.springcloudzuul.common;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author：ZhuWenming
+ * @CreateTime：2019/9/27 11:06
+ * @Description：TODO
+ * @Version: V1.0.0
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    /**
+     * @Author: ZhuWenming
+     * @Description: 返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+     * pre：路由之前
+     * routing：路由之时
+     * post： 路由之后
+     * error：发送错误调用
+     * @CreateTime: 2019/9/27 11:09
+     * @Param: null
+     * @Return:
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    //过滤的顺序
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    //这里可以写逻辑判断，是否要过滤，true永远过滤。
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    //过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问。
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
+
