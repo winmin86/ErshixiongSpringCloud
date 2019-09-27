04f0e50a97fce8331da27e21eeb23633af722537
zhuwenming <zhuwenming@ybm100.com>
2019-09-27 14:56:11 +0800

    config集群模式

--- spring-cloud-config-client/src/main/java/xyz/up123/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/xyz/up123/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -3,11 +3,13 @@ package xyz.up123.springcloudconfigclient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class SpringCloudConfigClientApplication {
 
     public static void main(String[] args) {
